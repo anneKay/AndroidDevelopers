5cc68d7b8fe188ea6645b721d2fad15fbe0b3bd8
Annekay <nwannauk@gmail.com>
2017-08-29 04:13:02 +0100

    implements swipe refresh layout

--- app/src/main/java/com/annekay/android/androiddevelopers/DevelopersListFragment.java
+++ app/src/main/java/com/annekay/android/androiddevelopers/DevelopersListFragment.java
@@ -16,8 +16,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -36,10 +39,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.mode;
+import static android.R.attr.orientation;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DevelopersListFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Developer>>{
+public class DevelopersListFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Developer>>, SwipeRefreshLayout.OnRefreshListener {
     private static final String JAVA_DEVELOPER = "https://api.github.com/search/users?q=language:java+location:lagos&page=1&per_page=100";
     private static final String JAVA_DEVELOPERS = "https://api.github.com/search/users?page=1&per_page=100";
     /** Tag for the log messages */
@@ -48,9 +54,11 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
     private static String devLocation;
     private static final int JSON_LOADER_ID = 1;
     ProgressBar developerProgressBar;
-    public String thumbNailUrl, userName, gitHubUrl, userNameDetail, gitHubUrlDetails;
+    public String userNameDetail, gitHubUrlDetails;
     ListView listView;
     TextView feedbackView;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
 
 
     public DevelopersListFragment() {
@@ -69,13 +77,16 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
 
         feedbackView = (TextView) rootView.findViewById(android.R.id.empty);
         developerProgressBar = (ProgressBar)rootView.findViewById(R.id.determinateBar);
+        mSwipeRefreshLayout = (SwipeRefreshLayout)rootView.findViewById(R.id.swipe_refresh_layout);
+        //apply different colors to the swipe refresh layout
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
 
         listView = (ListView) rootView.findViewById(R.id.list);
 
         //initialize the developersAdapter
         developersAdapter = new DevelopersAdapter(getActivity(), new ArrayList<Developer>());
         listView.setAdapter(developersAdapter);
-
+        mSwipeRefreshLayout.setOnRefreshListener(this);
         detectNetworkState();
 
          /*
@@ -106,65 +117,55 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
                     // Getting the fragment manager
 
 
-                    /** Getting the fragmenttransaction object, which can be used to add, remove or replace a fragment */
+                    // Getting the fragmenttransaction object, which can be used to add, remove or replace a fragment */
                     FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
 
-                    /** Getting the existing detailed fragment object, if it already exists.
+                    /* Getting the existing detailed fragment object, if it already exists.
                      *  The fragment object is retrieved by its tag name
                      * */
                     Fragment prevFrag = getFragmentManager().findFragmentByTag("developers.details");
 
-                    /** Remove the existing detailed fragment object if it exists */
+                    // Remove the existing detailed fragment object if it exists */
                     if(prevFrag!=null)
                         fragmentTransaction.remove(prevFrag);
 
-                    /** Instantiating the fragment CountryDetailsFragment */
+                    // Instantiating the fragment CountryDetailsFragment */
                     DeveloperDetailsFragment fragment = new DeveloperDetailsFragment();
 
-                    /** Creating a bundle object to pass the data(the clicked item's position) from the activity to the fragment */
+                    // Creating a bundle object to pass the data(the clicked item's position) from the activity to the fragment */
                     Bundle b = new Bundle();
 
-                    /** Setting the data to the bundle object */
+                    //Setting the data to the bundle object */
 
                     b.putString("userName", userNameDetail);
                     b.putString("gitHubUrl", gitHubUrlDetails);
                     b.putByteArray("image", imageByte);
-                    //b.putString("profileImage", bitmap);
-                    //b.putString("profileImage", currentDeveloper.getThumbnailUrl());
-//                    developer.setGitHubUrl(currentDeveloper.getGitHubUrl());
-//                    developer.setThumbnailUrl(currentDeveloper.getThumbnailUrl());
-//                    developer.setUserName(currentDeveloper.getUserName());
 
-                    /** Setting the bundle object to the fragment */
+                    // Setting the bundle object to the fragment */
                     fragment.setArguments(b);
 
-                    /** Adding the fragment to the fragment transaction */
+                    // Adding the fragment to the fragment transaction */
                     fragmentTransaction.add(R.id.detail_fragment_container, fragment,"developers.details");
 
-                    /** Adding this transaction to backstack */
+                    // Adding this transaction to backstack */
                     fragmentTransaction.addToBackStack(null).commit();
 
-                    /** Making this transaction in effect */
-                    // fragmentTransaction.commit();
+
+
                 }else{
-                    /** Portrait Mode or Square mode */
-                    /** Creating an intent object to start the CountryDetailsActivity */
+                    // In Portrait Mode or Square mode */
+                    // Creating an intent object to start the CountryDetailsActivity */
                     Intent intent = new Intent(getActivity(), DeveloperDetails.class);
 
-                    /** Setting data ( the clicked item's position ) to this intent */
+                    // Setting data ( the clicked item's position ) to this intent */
 
                     intent.putExtra("userName", currentDeveloper.getUserName());
                     intent.putExtra("gitHubUrl", currentDeveloper.getGitHubUrl());
                     intent.putExtra("image", imageByte);
-                    // intent.putExtra("imageUrl", currentDeveloper.getThumbnailUrl());
-
-                    /** Starting the activity by passing the implicit intent */
                     startActivity(intent);
                 }
             }
         });
-        Toast.makeText(getContext(), "you are" + getDevLocation() + ", " +
-                "You scored a total of " + " out of 6", Toast.LENGTH_LONG).show();
 
 
 
@@ -181,23 +182,10 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
                 getString(R.string.settings_dev_location_default));
         Uri baseUri = Uri.parse(JAVA_DEVELOPERS);
         Uri.Builder uriBuilder = baseUri.buildUpon();
-        Toast.makeText(getContext(),  devLocation + ", " +
-                "You scored a total of " + " out of 6", Toast.LENGTH_LONG).show();
-
-
         uriBuilder.appendQueryParameter("q", "language:java location:"+devLocation);
-        //uriBuilder.appendQueryParameter("page", "3");
-//        uriBuilder.appendQueryParameter("location", devLocation);
-//        uriBuilder.appendQueryParameter("page", "3");
-      // uriBuilder.appendQueryParameter("per_page", "100");
-//
-//        uriBuilder.appendPath("q");
-
         return new JsonLoader(getActivity(), uriBuilder.toString());
         // TODO: Create a new loader for the given URL
         //return new JsonLoader(getActivity(), JAVA_DEVELOPER);
-
-
     }
     @Override
 
@@ -208,6 +196,15 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
         if (data != null && !data.isEmpty()) {
 //
             developersAdapter.addAll(data);
+            mSwipeRefreshLayout.post(new Runnable() {
+                @Override
+                public void run() {
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+            });
+//            listView.setSelection(0);
+//            listView.getSelectedView().setSelected(true);
+
         }else {
             listView.setEmptyView(getView().findViewById(android.R.id.empty));
         }
@@ -219,6 +216,11 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
 
         // TODO: Loader reset, so we can clear out our existing data.
         developersAdapter.clear();
+    }
+
+    @Override
+    public void onRefresh() {
+        detectNetworkState();
     }
 
     @Override
@@ -238,14 +240,18 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
             getLoaderManager().initLoader(JSON_LOADER_ID, null, this).forceLoad();
         }else {
             feedbackView.setVisibility(View.VISIBLE);
+            feedbackView.setText("No Internt connection");
             progressBar();
         }
     }
     public void progressBar() {
         developerProgressBar.setVisibility(View.GONE);
     }
+
+   //inflate the menu item if it is present
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
         inflater.inflate(R.menu.main, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -264,7 +270,4 @@ public class DevelopersListFragment extends Fragment implements LoaderManager.Lo
     }
 
 
-    public void setDevLocation(String devLocation) {
-        this.devLocation = devLocation;
-    }
 }
